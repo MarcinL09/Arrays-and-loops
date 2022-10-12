7f2a1f103b51b435e8bd85778c318af55a67773e
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-10-12 16:55:48 +0200

    printed all elements of the vegetables array to the console using the for loop and while loop

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ const fruits = ["Orange", "Apple", "Lemon"]
 
 console.log(fruits[fruits.length - 1]);
 
+// print all elements of the vegetables array to the console using the while loop.
+
+const vegetables = ['Carrot', 'Tomato', 'Cabbage'];
+
+let index = 0;
+while (index < 3) {
+    console.log(vegetables[index]);
+    index = index + 1;
+}
+
+// print all elements of the vegetables array to the console using the for loop.
+
+for (let i = 0; i < 3; ++i) {
+    console.log(vegetables[i]);
+}
