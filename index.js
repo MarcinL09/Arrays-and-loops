0d577bbcced34199eb62c74830db29ff7624a2ad
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-10-12 07:55:07 +0200

    added 'fruits' array

---
+++ index.js
@@ -0,0 +1,8 @@
+// create an array with three fruits
+
+const fruits = ["Orange", "Apple", "Lemon"]
+
+// print the last element in the fruits  array to the console
+
+console.log(fruits[fruits.length - 1]);
+
